cfed5a40eb181b07fa1896aad8c88f4c95280cdf
Abhijeet Pal <abhijeetpal0307@gmail.com>
2026-08-16 19:46:44 +0530

    Setup Vite ,Tailwind and Routing

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Body from "./Body";
+import Login from "./Login";
+import Navbar from "./Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./Profile";
+export default function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body/>} >
+          <Route path="/login" element={<Login/>} />
+          <Route path="/profile" element={<Profile/>} />
+         </Route>
+        </Routes>
+      </BrowserRouter>
+   
+    </>
+  );
+}
